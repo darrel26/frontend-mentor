62d1687a7911789add597194acc0f2661bf3b35a
Noid <dionisiusgunadi@outlook.com>
2023-03-03 13:50:01 +0700

    initial commit

---
+++ qr-code/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="card-container">
+      <img src="src\assets\images\image-qr-code.png"></img>
+      <h2>Improve your front-end skills by building project</h2>
+      <p>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
+    </div>
+  );
+}
+
+export default App;
